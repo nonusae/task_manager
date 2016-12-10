0196cfd4d984285e34499ea14728e5ef60d2b0d1
Natthakit <nonusae@gmail.com>
2016-12-10 22:59:08 +0700

    final draft v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
+User.create(email: "test@test.com",password:"asdfgh",password_confirmation:"asdfgh")
+User.create(email: "test_again@test.com",password:"asdfgh",password_confirmation:"asdfgh")
+User.create(email: "forever@reward.com",password:"asdfgh",password_confirmation:"asdfgh")
+User.create(email: "shipnity@test_test.com",password:"asdfgh",password_confirmation:"asdfgh")
 
 
-Task.create(description: "Open this mock-up in Adobe Fireworks")
-Task.create(description: "Attentively check the file")
-Task.create(description: "Write HTML & CSS")
-Task.create(description: "Add Javascript to implement adding / editing / removing for todo items and list taking into account as more uses cases as possible")
+
+
+
+Task.create(description: "Open this mock-up in Adobe Fireworks",deadline: 5.days.from_now, priority: 10, user_id: 1)
+Task.create(description: "Attentively check the file",deadline: 10.days.from_now ,priority: 2, user_id: 2)
+Task.create(description: "Write HTML & CSS", deadline: 3.days.ago ,priority: 5, user_id: 1)
+Task.create(description: "Add Javascript to implement adding / editing / removing for todo items and list taking into account as more uses cases as possible",deadline: 11.days.from_now, priority: 3, user_id: 3)
